bb51996472a48a98b989b2e3e0e21080d46cae16
Chuseok22 <bjh59629@naver.com>
2025-11-10 16:28:45 +0900

    root_layout_설정 : feat : 기본 레이아웃 설정 https://github.com/Chuseok22/flash-talk-web/issues/1

---
+++ src/app/viewport.ts
@@ -0,0 +1,10 @@
+import { Viewport } from "next";
+
+const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  viewportFit: 'cover',
+  themeColor: '#ffffff',
+}
+
+export default viewport;
